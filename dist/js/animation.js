7f906e700ea2cba08aefed097e93e5af00cb82cc
Gulam Qasim <gfram@hotmail.co.uk>
2020-05-18 16:57:03 +0100

    Updated Menu-Title Animation

--- dist/js/animation.js
+++ dist/js/animation.js
@@ -1,27 +1,28 @@
-var textWrapper = document.querySelector('.ml14 .letters');
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
-.add({
-targets: '.ml14 .line',
-scaleX: [0,1],
-opacity: [0.5,1],
-easing: "easeInOutExpo",
-duration: 900
-}).add({
-targets: '.ml14 .letter',
-opacity: [0,1],
-translateX: [40,0],
-translateZ: 0,
-scaleX: [0.3, 1],
-easing: "easeOutExpo",
-duration: 800,
-offset: '-=600',
-delay: (el, i) => 150 + 25 * i
-}).add({
-targets: '.ml14',
-opacity: 0,
-duration: 1000,
-easing: "easeOutExpo",
-delay: 1000
-});
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  })
